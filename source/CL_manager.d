3040e9a27e3e6ac273ccdf6fcaefd8119362aa1e
khashayarAPonVoidLinux <aripastudio44@gmail.com>
2025-05-30 18:44:06 +0330

    add File

---
+++ source/CL_manager.d
@@ -0,0 +1,21 @@
+module CL_manager;
+
+class CL_manager_Ap
+{
+    public void ScreenShot()
+    {
+
+    }
+
+    public void ScreenShot_Copy()
+    {
+
+    }
+
+    public void ScreenShot_Save()
+    {
+        
+    }
+
+    
+}
