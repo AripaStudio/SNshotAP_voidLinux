f083d6495fe05633a2d8cd3ba34ea57d445a6ef6
khashayarAPonVoidLinux <aripastudio44@gmail.com>
2025-05-30 18:31:19 +0330

    Start Project

---
+++ source/app.d
@@ -0,0 +1,16 @@
+import std.stdio;
+
+
+    /** 
+     SNshotAP 
+     Programmer : khashayar Mobasheri (Abolfazl)
+     Studio : Aripa Studio (Aripa Pars Studio)
+     Start at : 2025-5-30;
+     (Iran) 1404-3-9;
+     end V1 at : null;
+     */
+     
+void main()
+{
+
+}
